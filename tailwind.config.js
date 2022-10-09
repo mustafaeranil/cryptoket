33f1908d9b7a10c6f7d38da35faf0ec546f6f7b1
Mustafa ERANIL <mustafaeranil2@gmail.com>
2022-10-09 15:18:55 +0300

    feat: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ["./src/**/*.vue", "./public/index.html"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          100: "#DA18A3",
+          200: "#EB1484",
+          300: "#C81CC5"
+        },
+        grey: {
+          100: "#E3E1E3",
+          200: "#888888",
+          300: "#4F4F4F"
+        },
+        dark: {
+          bg: "#24252D",
+          100: "#2D2E36",
+          200: "#1B1A21",
+          300: "#2A2D3A",
+          400: "#24252D"
+        }
+      }
+    }
   },
   plugins: []
 }
