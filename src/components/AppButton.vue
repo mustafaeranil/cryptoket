24bfb92381bb620cb6a9d3154df6c71112198af1
Mustafa ERANIL <mustafaeranil2@gmail.com>
2022-10-09 17:56:15 +0300

    feat: add AppButton component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button :class="buttonClasses">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonStyle: {
+      type: String,
+      default: "filled",
+      validator(val) {
+        return ["filled", "stroke"].includes(val)
+      }
+    }
+  },
+  computed: {
+    buttonClasses() {
+      return ["app-button", `app-button__${this.buttonStyle}`]
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+.app-button {
+  @apply rounded-xl px-7 py-2 text-sm font-semibold;
+
+  &__filled {
+    @apply bg-gradient-to-r from-primary-200 to-primary-300 text-white;
+  }
+
+  &__stroke {
+    @apply border border-primary-100 text-primary-100;
+  }
+}
+</style>
