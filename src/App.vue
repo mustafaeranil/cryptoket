f6a57b6c0879657d6c9e49f2f87d52f982c993fa
Mustafa ERANIL <mustafaeranil2@gmail.com>
2022-10-09 22:57:39 +0300

    feat: add HomeCover component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app" class="dark:bg-dark-bg">
-    <div class="flex justify-center border-b border-[#E3E1E3]">
+    <div class="mb-4 flex justify-center border-b border-[#E3E1E3] lg:mb-14">
       <AppHeader class="w-full max-w-screen-xl" />
     </div>
-    <section class=""></section>
+    <section class="app-section">
+      <div class="app-container">
+        <HomeCover />
+      </div>
+    </section>
+    <section class="app-section">
+      <div class="app-container">
+        <h2 class="section-header">Top Sellers</h2>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue"
+import HomeCover from "./components/HomeCover.vue"
 export default {
   name: "App",
   components: {
-    AppHeader
+    AppHeader,
+    HomeCover
   }
 }
 </script>
---
+++ src/components/HomeCover.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home-cover">
+    <div class="home-cover__circle home-cover__circle--left"></div>
+    <div class="home-cover__circle home-cover__circle--right"></div>
+    <div class="home-cover__text">
+      Discover, collect, and sell extraordinary NFTs
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeCover"
+}
+</script>
+
+<style lang="postcss">
+.home-cover {
+  @apply relative flex h-36 w-full items-center overflow-hidden rounded-3xl bg-gradient-to-r from-primary-200 to-primary-300 lg:h-[300px];
+
+  &__circle {
+    @apply absolute h-24 w-24 rounded-full bg-white opacity-20 lg:h-80 lg:w-80;
+
+    &--left {
+      @apply -left-12 -top-12 lg:-left-40 lg:-top-40;
+    }
+    &--right {
+      @apply -right-7 -bottom-7 lg:-right-9 lg:-bottom-32;
+    }
+  }
+
+  &__text {
+    @apply px-5 font-gotham text-xl font-bold leading-none text-white lg:max-w-3xl lg:px-14 lg:text-5xl;
+  }
+}
+</style>
